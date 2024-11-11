a42d082aa94d34766452dc4d05104f0dcec77940
eljohn cuaresma <eljohn.cuaresma6@gmail.com>
2024-11-11 21:07:45 +0800

    add event fix

--- dbconfig.php
+++ dbconfig.php
@@ -4,6 +4,11 @@ class Database {
     private $db_name = "tuteefind";
     private $username = "tuteefind";
     private $password = "tutee_1234Find";
+
+    // private $host = "localhost";
+    // private $db_name = "tuteefind";
+    // private $username = "root";
+    // private $password = "";
     public $conn;
 
     public function dbConnection() {
