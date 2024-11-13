cb118b2425867099ddb4541b72e9564772eed930
eljohn cuaresma <eljohn.cuaresma6@gmail.com>
2024-11-13 22:19:19 +0800

    registration

--- admin/restore_tutee.php
+++ admin/restore_tutee.php
@@ -12,8 +12,8 @@ if(isset($_POST['restoreTutee'])){
         $row = $query->fetch_assoc();
 
         // Insert back into the tutee table
-        $sql_restore = "INSERT INTO tutee (firstname, lastname, age, sex, number, guardianname, fblink, barangay, tutee_bday, school, grade, emailaddress, photo, password) 
-                        VALUES ('".$row['firstname']."', '".$row['lastname']."', '".$row['age']."', '".$row['sex']."', '".$row['number']."', '".$row['guardianname']."', '".$row['fblink']."', '".$row['barangay']."', '".$row['tutee_bday']."', '".$row['school']."', '".$row['grade']."', '".$row['emailaddress']."', '".$row['photo']."', '".$row['password']."')";
+        $sql_restore = "INSERT INTO tutee (firstname, lastname, age, sex, number, guardianname, fblink, barangay, tutee_bday, school, grade, emailaddress, photo, password, bio) 
+                        VALUES ('".$row['firstname']."', '".$row['lastname']."', '".$row['age']."', '".$row['sex']."', '".$row['number']."', '".$row['guardianname']."', '".$row['fblink']."', '".$row['barangay']."', '".$row['tutee_bday']."', '".$row['school']."', '".$row['grade']."', '".$row['emailaddress']."', '".$row['photo']."', '".$row['password']."', '".$row['bio']."')";
 
         if($conn->query($sql_restore)){
             // Delete from the archive_tutee table after restoring
