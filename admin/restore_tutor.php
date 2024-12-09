9b4f39d82656d534a245c182ae41fdc49577e7e7
Raescils <itsclarissedizon@gmail.com>
2024-12-09 19:09:08 +0800

    fixes

--- admin/restore_tutor.php
+++ admin/restore_tutor.php
@@ -15,125 +15,147 @@ if (isset($_POST['restore'])) {
         if ($query->num_rows > 0) {
             $row = $query->fetch_assoc();
 
-            // Insert back into the tutor table
-            $sql_restore = "INSERT INTO tutor (id, lastname, firstname, age, sex, number, barangay, student_id, course, year_section, professor, fblink, emailaddress, password, bio, last_login) 
-                VALUES ('".$row['id']."', '".$row['lastname']."', '".$row['firstname']."', '".$row['age']."', '".$row['sex']."', '".$row['number']."', '".$row['barangay']."', '".$row['student_id']."', '".$row['course']."', '".$row['year_section']."', '".$row['professor']."', '".$row['fblink']."', '".$row['emailaddress']."', '".$row['password']."', '".$row['bio']."', '".$row['last_login']."')";
+            // Prepare the insert statement for the tutor table
+            $stmt_restore = $conn->prepare("INSERT INTO tutor (id, lastname, firstname, age, sex, number, barangay, student_id, course, year_section, professor, fblink, emailaddress, password, bio, last_login) 
+                                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-            if ($conn->query($sql_restore)) {
-                // Restore related data from each table manually
+            // Bind the parameters for the insert
+            $stmt_restore->bind_param(
+                "ississssssssssss", // Types of the parameters
+                $row['id'],        // id
+                $row['lastname'],  // lastname
+                $row['firstname'], // firstname
+                $row['age'],       // age
+                $row['sex'],       // sex
+                $row['number'],    // number
+                $row['barangay'],  // barangay
+                $row['student_id'],// student_id
+                $row['course'],    // course
+                $row['year_section'], // year_section
+                $row['professor'], // professor
+                $row['fblink'],    // fblink
+                $row['emailaddress'], // emailaddress
+                $row['password'],  // password
+                $row['bio'],       // bio
+                $row['last_login'] // last_login
+            );
 
-                // Restore requests table
-                $sql_restore_requests = "INSERT INTO requests SELECT * FROM archive_requests WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_requests)) {
-                    throw new Exception('Error restoring requests: ' . $conn->error);
-                }
-
-                // Restore tutee_progress table
-                $sql_restore_tutee_progress = "INSERT INTO tutee_progress SELECT * FROM archive_tutee_progress WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_tutee_progress)) {
-                    throw new Exception('Error restoring tutee_progress: ' . $conn->error);
-                }
-
-                // Restore tutor_ratings table
-                $sql_restore_tutor_ratings = "INSERT INTO tutor_ratings SELECT * FROM archive_tutor_ratings WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_tutor_ratings)) {
-                    throw new Exception('Error restoring tutor_ratings: ' . $conn->error);
-                }
-
-                // Restore tutor_sessions table
-                $sql_restore_tutor_sessions = "INSERT INTO tutor_sessions SELECT * FROM archive_tutor_sessions WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_tutor_sessions)) {
-                    throw new Exception('Error restoring tutor_sessions: ' . $conn->error);
-                }
-
-                // Restore tutee_summary table
-                $sql_restore_tutee_summary = "INSERT INTO tutee_summary SELECT * FROM archive_tutee_summary WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_tutee_summary)) {
-                    throw new Exception('Error restoring tutee_summary: ' . $conn->error);
-                }
-
-                // Restore events table
-                $sql_restore_events = "INSERT INTO events SELECT * FROM archive_events WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_events)) {
-                    throw new Exception('Error restoring events: ' . $conn->error);
-                }
-
-                // Restore messages table
-                $sql_restore_messages = "INSERT INTO messages SELECT * FROM archive_messages WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_messages)) {
-                    throw new Exception('Error restoring messages: ' . $conn->error);
-                }
-
-                // Restore notifications table
-                $sql_restore_notifications = "INSERT INTO notifications SELECT * FROM archive_notifications WHERE receiver_id = '$id' AND sent_for = 'tutor'";
-                if (!$conn->query($sql_restore_notifications)) {
-                    throw new Exception('Error restoring notifications: ' . $conn->error);
-                }
-
-                // Restore tutor_logs table
-                $sql_restore_tutor_logs = "INSERT INTO tutor_logs SELECT * FROM archive_tutor_logs WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_restore_tutor_logs)) {
-                    throw new Exception('Error restoring tutor_logs: ' . $conn->error);
-                }
-
-                // Delete the restored data from archive tables
-                $sql_delete_requests = "DELETE FROM archive_requests WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_requests)) {
-                    throw new Exception("Error deleting requests from archive: " . $conn->error);
-                }
-
-                $sql_delete_tutee_progress = "DELETE FROM archive_tutee_progress WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_tutee_progress)) {
-                    throw new Exception("Error deleting tutee_progress from archive: " . $conn->error);
-                }
-
-                $sql_delete_tutor_ratings = "DELETE FROM archive_tutor_ratings WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_tutor_ratings)) {
-                    throw new Exception("Error deleting tutor_ratings from archive: " . $conn->error);
-                }
-
-                $sql_delete_tutor_sessions = "DELETE FROM archive_tutor_sessions WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_tutor_sessions)) {
-                    throw new Exception("Error deleting tutor_sessions from archive: " . $conn->error);
-                }
-
-                $sql_delete_tutee_summary = "DELETE FROM archive_tutee_summary WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_tutee_summary)) {
-                    throw new Exception("Error deleting tutee_summary from archive: " . $conn->error);
-                }
-
-                $sql_delete_events = "DELETE FROM archive_events WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_events)) {
-                    throw new Exception("Error deleting events from archive: " . $conn->error);
-                }
-
-                $sql_delete_messages = "DELETE FROM archive_messages WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_messages)) {
-                    throw new Exception("Error deleting messages from archive: " . $conn->error);
-                }
-
-                $sql_delete_notifications = "DELETE FROM archive_notifications WHERE receiver_id = '$id' AND sent_for = 'tutor'";
-                if (!$conn->query($sql_delete_notifications)) {
-                    throw new Exception("Error deleting notifications from archive: " . $conn->error);
-                }
-
-                $sql_delete_tutor_logs = "DELETE FROM archive_tutor_logs WHERE tutor_id = '$id'";
-                if (!$conn->query($sql_delete_tutor_logs)) {
-                    throw new Exception("Error deleting tutor_logs from archive: " . $conn->error);
-                }
-
-                // Delete the tutor from the archive_tutor table
-                $sql_delete_tutor = "DELETE FROM archive_tutor WHERE id = '$id'";
-                if (!$conn->query($sql_delete_tutor)) {
-                    throw new Exception("Error deleting tutor from archive_tutor: " . $conn->error);
-                }
-
-                // Commit the transaction
-                $conn->commit();
-                $_SESSION['success'] = 'Tutor and related data restored successfully';
-            } else {
-                throw new Exception('Error restoring tutor: ' . $conn->error);
+            // Execute the insert statement
+            if (!$stmt_restore->execute()) {
+                throw new Exception("Error restoring tutor: ".$conn->error);
             }
+
+            $tutor_id = $row['id']; // Store the tutor's ID for restoring related data
+
+            // Restore related data for this tutor
+            // Restore requests
+            $sql_restoreRequests = "INSERT INTO requests (request_id, tutor_id, tutee_id, status)
+                                    SELECT request_id, tutor_id, tutee_id, status FROM archive_requests WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreRequests)) {
+                throw new Exception('Error restoring requests: ' . $conn->error);
+            }
+            $sql_deleteRequests = "DELETE FROM archive_requests WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteRequests)) {
+                throw new Exception("Error deleting requests from archive: " . $conn->error);
+            }
+
+            // Restore tutee progress
+            $sql_restoreProgress = "INSERT INTO tutee_progress (id, tutee_id, tutor_id, week_number, uploaded_files, description, date)
+                                    SELECT id, tutee_id, tutor_id, week_number, uploaded_files, description, date FROM archive_tutee_progress WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreProgress)) {
+                throw new Exception('Error restoring tutee_progress: ' . $conn->error);
+            }
+            $sql_deleteProgress = "DELETE FROM archive_tutee_progress WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteProgress)) {
+                throw new Exception("Error deleting tutee_progress from archive: " . $conn->error);
+            }
+
+            // Restore tutor ratings
+            $sql_restoreRatings = "INSERT INTO tutor_ratings (id, tutee_id, tutor_id, rating, comment, pdf_content)
+                                   SELECT id, tutee_id, tutor_id, rating, comment, pdf_content FROM archive_tutor_ratings WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreRatings)) {
+                throw new Exception('Error restoring tutor_ratings: ' . $conn->error);
+            }
+            $sql_deleteRatings = "DELETE FROM archive_tutor_ratings WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteRatings)) {
+                throw new Exception("Error deleting tutor_ratings from archive: " . $conn->error);
+            }
+
+            // Restore tutor sessions
+            $sql_restoreSessions = "INSERT INTO tutor_sessions (id, tutor_id, tutee_id, status)
+                                    SELECT id, tutor_id, tutee_id, status FROM archive_tutor_sessions WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreSessions)) {
+                throw new Exception('Error restoring tutor_sessions: ' . $conn->error);
+            }
+            $sql_deleteSessions = "DELETE FROM archive_tutor_sessions WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteSessions)) {
+                throw new Exception("Error deleting tutor_sessions from archive: " . $conn->error);
+            }
+
+            // Restore tutee summary
+            $sql_restoreSummary = "INSERT INTO tutee_summary (tutee_id, tutor_id, completed_weeks, registered_weeks)
+                                    SELECT tutee_id, tutor_id, completed_weeks, registered_weeks FROM archive_tutee_summary WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreSummary)) {
+                throw new Exception('Error restoring tutee_summary: ' . $conn->error);
+            }
+            $sql_deleteSummary = "DELETE FROM archive_tutee_summary WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteSummary)) {
+                throw new Exception("Error deleting tutee_summary from archive: " . $conn->error);
+            }
+
+            // Restore events
+            $sql_restoreEvents = "INSERT INTO events (id, tutor_id, event_name, rendered_hours, description, attached_file, created_at, status, remarks)
+                                  SELECT id, tutor_id, event_name, rendered_hours, description, attached_file, created_at, status, remarks FROM archive_events WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreEvents)) {
+                throw new Exception('Error restoring events: ' . $conn->error);
+            }
+            $sql_deleteEvents = "DELETE FROM archive_events WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteEvents)) {
+                throw new Exception("Error deleting events from archive: " . $conn->error);
+            }
+
+            // Restore messages
+            $sql_restoreMessages = "INSERT INTO messages (id, tutor_id, tutee_id, sender_type, message, created_at, is_read)
+                                    SELECT id, tutor_id, tutee_id, sender_type, message, created_at, is_read FROM archive_messages WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreMessages)) {
+                throw new Exception('Error restoring messages: ' . $conn->error);
+            }
+            $sql_deleteMessages = "DELETE FROM archive_messages WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteMessages)) {
+                throw new Exception("Error deleting messages from archive: " . $conn->error);
+            }
+
+            // Restore notifications
+            $sql_restoreNotifications = "INSERT INTO notifications (id, sender_id, receiver_id, title, message, status, date_sent, sent_for)
+                                         SELECT id, sender_id, receiver_id, title, message, status, date_sent, sent_for FROM archive_notifications WHERE receiver_id = '$tutor_id' AND sent_for = 'tutor'";
+            if (!$conn->query($sql_restoreNotifications)) {
+                throw new Exception('Error restoring notifications: ' . $conn->error);
+            }
+            $sql_deleteNotifications = "DELETE FROM archive_notifications WHERE receiver_id = '$tutor_id' AND sent_for = 'tutor'";
+            if (!$conn->query($sql_deleteNotifications)) {
+                throw new Exception("Error deleting notifications from archive: " . $conn->error);
+            }
+
+            // Restore tutor logs
+            $sql_restoreLogs = "INSERT INTO tutor_logs (id, tutor_id, activity, datetime)
+                               SELECT id, tutor_id, activity, datetime FROM archive_tutor_logs WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_restoreLogs)) {
+                throw new Exception('Error restoring tutor logs: ' . $conn->error);
+            }
+            $sql_deleteLogs = "DELETE FROM archive_tutor_logs WHERE tutor_id = '$tutor_id'";
+            if (!$conn->query($sql_deleteLogs)) {
+                throw new Exception("Error deleting tutor logs from archive: " . $conn->error);
+            }
+
+            // Delete the tutor from archive_tutor table after restoring
+            $sql_deleteTutor = "DELETE FROM archive_tutor WHERE id = '$tutor_id'";
+            if (!$conn->query($sql_deleteTutor)) {
+                throw new Exception("Error deleting tutor from archive_tutor: " . $conn->error);
+            }
+
+            // Commit the transaction
+            $conn->commit();
+            $_SESSION['success'] = 'Tutor and related data restored successfully';
         } else {
             throw new Exception('Tutor not found in archive');
         }
